bc69f2e76df5fcca3b469db2433cfb768bab5a9d
Carmen Romano <my-9@hotmail.it>
2024-03-21 16:14:16 +0100

    feedback page

--- scripts/feedbackpage.js
+++ scripts/feedbackpage.js
@@ -1,51 +1,58 @@
-const starContainer = document.getElementById("starContainer")
-const stars = document.querySelectorAll(".star")
-const feedBtn = document.getElementById("feedBtn")
-const starsArray = Array.from(stars)
-let starIndex = 0
+const starContainer = document.getElementById("starContainer");
+const stars = document.querySelectorAll(".star");
+const feedBtn = document.getElementById("feedBtn");
+const starsArray = Array.from(stars);
+let starIndex = 0;
 
 const handleEnterStar = (e) => {
-  const index = getIndex(e.srcElement)
-  starIndex = index
+  const index = getIndex(e.srcElement);
+  starIndex = index;
 
   for (let j = 0; j <= index; j++) {
-    starsArray[j].childNodes[1].style.fill = "#00FFFF"
+    starsArray[j].childNodes[1].style.fill = "#00FFFF";
   }
 
   for (let k = index + 1; k < starsArray.length; k++) {
-    starsArray[k].childNodes[1].style.fill = "#5f2b73"
+    starsArray[k].childNodes[1].style.fill = "#5f2b73";
   }
-}
+};
 
 for (let i = 0; i < starsArray.length; i++) {
-  console.log(starsArray[i])
-  starsArray[i].addEventListener("mouseenter", handleEnterStar)
+  console.log(starsArray[i]);
+  starsArray[i].addEventListener("mouseenter", handleEnterStar);
 }
 
-const getIndex = (element) => starsArray.indexOf(element)
+const getIndex = (element) => starsArray.indexOf(element);
 
 const handleClickBtn = () => {
-  const messageContainer = document.getElementById("messageContainer")
-  const page = document.getElementById("pageContainer")
-  let message = ""
+  const messageContainer = document.getElementById("messageContainer");
+  const page = document.getElementById("pageContainer");
+
+  let message = "";
   if (starIndex < 6) {
     message =
-      "We're truly sorry to hear about your experience. Your feedback is invaluable to us, and we're committed to addressing the issues you've encountered. Please reach out to us directly so we can make things right."
+      "We're truly sorry to hear about your experience. Your feedback is invaluable to us, and we're committed to addressing the issues you've encountered. Please reach out to us directly so we can make things right.";
   } else if (starIndex < 8) {
     message =
-      "Thank you for sharing your feedback with us. We're glad to hear that your experience was satisfactory, but we're always striving to improve. If there's anything specific you think we could do better, please let us know."
+      "We're glad to hear that your experience was satisfactory, but we're always striving to improve. If there's anything specific you think we could do better, please let us know.";
   } else {
     message =
-      "Thank you so much for your glowing review! We're thrilled to hear that you had such a great experience. Your kind words motivate us to continue delivering excellent service. We look forward to serving you again in the future!"
+      "We're thrilled to hear that you had such a great experience. Your kind words motivate us to continue delivering excellent service. We look forward to serving you again in the future!";
   }
 
-  page.hidden = true
+  page.hidden = true;
 
-  const messageP = document.createElement("p")
-  messageP.innerText = message
-  messageContainer.appendChild(messageP)
-}
+  let thankYouText = document.createElement("h3");
+  thankYouText.className = "thankYou";
+  thankYouText.innerText = `Thank you for your review!`;
+  messageContainer.appendChild(thankYouText);
+
+  const messageP = document.createElement("p");
+  messageP.innerText = message;
+
+  messageContainer.appendChild(messageP);
+};
 
 window.onload = function () {
-  feedBtn.addEventListener("click", handleClickBtn)
-}
+  feedBtn.addEventListener("click", handleClickBtn);
+};
