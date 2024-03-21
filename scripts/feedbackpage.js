8121f1218a365803be054900122f94ce432edb54
Carmen Romano <my-9@hotmail.it>
2024-03-21 13:04:50 +0100

    Miglioramenti vari, allineamento loghi e modificato colore stelle

--- scripts/feedbackpage.js
+++ scripts/feedbackpage.js
@@ -1,22 +1,22 @@
-let starContainer = document.getElementById("starContainer")
-let stars = document.querySelectorAll(".star")
-const starsArray = Array.from(stars)
+let starContainer = document.getElementById("starContainer");
+let stars = document.querySelectorAll(".star");
+const starsArray = Array.from(stars);
 
 const handleEnterStar = (e) => {
-  const index = getIndex(e.srcElement)
+  const index = getIndex(e.srcElement);
 
   for (let j = 0; j <= index; j++) {
-    starsArray[j].childNodes[1].style.fill = "#00FFFF"
+    starsArray[j].childNodes[1].style.fill = "#00FFFF";
   }
 
   for (let k = index + 1; k < starsArray.length; k++) {
-    starsArray[k].childNodes[1].style.fill = "#0b113b"
+    starsArray[k].childNodes[1].style.fill = "#5f2b73";
   }
-}
+};
 
 for (let i = 0; i < starsArray.length; i++) {
-  console.log(starsArray[i])
-  starsArray[i].addEventListener("mouseenter", handleEnterStar)
+  console.log(starsArray[i]);
+  starsArray[i].addEventListener("mouseenter", handleEnterStar);
 }
 
-const getIndex = (element) => starsArray.indexOf(element)
+const getIndex = (element) => starsArray.indexOf(element);
