1813258e564e9b793e591e5daeb2d9f52cec9fc0
Carmen Romano <my-9@hotmail.it>
2024-03-22 10:29:39 +0100

    btn fix

--- scripts/WelcomePage.js
+++ scripts/WelcomePage.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const checkbox = document.getElementById("check-b");
   const button = document.getElementById("button");
+
   checkbox.addEventListener("change", (event) => {
     if (checkbox.checked) {
       button.disabled = false;
-      button.classList = "btnBlueNeon bold abilited";
+      button.classList.remove("disabilited");
+      button.classList.add("abilited");
     } else {
       button.disabled = true;
-      button.classList = "btnBlueNeon bold disabilited";
+      button.classList.remove("abilited");
+      button.classList.add("disabilited");
     }
   });
+
   button.disabled = !checkbox.checked;
 });
